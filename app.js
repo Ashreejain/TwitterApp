258fe2722b9cf91ae6efd8d8ff648bc3599a0999
Ashreejain <40181680+Ashreejain@users.noreply.github.com>
2021-01-11 01:30:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 3003;
 const middleware = require('./middleware')
 const path = require('path')
 const bodyParser = require("body-parser")
